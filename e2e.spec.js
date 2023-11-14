9ea054cf1c6c3cfbd53b6fafb814e6303652e1af
Divyanshu <140911251+learneroflife30@users.noreply.github.com>
2023-11-14 15:26:43 +0530

    Add files via upload

--- e2e.spec.js
+++ e2e.spec.js
@@ -1,14 +1,20 @@
 const { test, expect, Playwright } = require("@playwright/test");
+const { count } = require("console");
 const path = require("path");
 
 test("end to end scenario", async ({ browser }) => {
+  // create new context with fresh page
   const context = await browser.newContext();
   const page = await context.newPage();
-  const email = "divyanshu75@gmail.com";
 
-  await page.goto("https://rahulshettyacademy.com/client");
+  // variables
+  const url = "https://rahulshettyacademy.com/client";
+  const email = "divyanshu75@gmail.com";
+  const password = "P~nN3IH!KSkg(`Gs";
+
+  await page.goto(url);
   await page.locator("#userEmail").fill(email);
-  await page.locator("#userPassword").fill("P~nN3IH!KSkg(`Gs");
+  await page.locator("#userPassword").fill(password);
   await page.locator("#login").click();
   await page.locator(".col-lg-4 .card b").first().waitFor();
   const products = await page.locator(".col-lg-4 .card b").allTextContents(); // returns array of multiple value
@@ -17,7 +23,7 @@ test("end to end scenario", async ({ browser }) => {
   console.log(producttoget);
   await page.locator(".btn.w-10.rounded").first().click();
   await page.locator("button[routerlink*='cart']").click();
-  await page.locator("h3").waitFor();
+  await page.locator("h3").first().waitFor();
   console.log(page.url());
   const productincart = await page.locator(".cart h3").textContent();
   console.log(productincart);
@@ -28,9 +34,17 @@ test("end to end scenario", async ({ browser }) => {
   const Email = await page.locator(".mt-5 [type='text']").first().textContent();
   console.log(Email);
   expect(email).toEqual(Email);
-  await page.locator('[placeholder="Select Country"]').fill("ind", { delay: 100 });
-  const country = await page
-    .locator(".ta-item .ng-star-inserted")
-    .textContent();
-  console.log(country)
+  await page.locator(".form-group .txt").pressSequentially("ind");
+  const button = page.locator(".list-group button").getByText(" India").last();
+  await button.click();
+  await page.locator(".actions .btnn").click();
+  console.log(page.url());
+  await page.pause();
+  await page.locator("td h1").textContent();
+  const orderId = await page.locator("td label").last().textContent();
+  console.log(orderId);
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator("tbody tr th").last().waitFor();
+  const orderids = await page.locator("tbody tr th").allTextContents();
+  console.log(orderids);
 });
